4635ea6c62353ce8268a9e4fc1b4be7700e94ff7
Kyle Robinson Young <kyle@dontkry.com>
2012-11-30 15:04:13 -0800

    List nodecopter modules from npm by name, description or keyword

--- pages/modules.md
+++ pages/modules.md
@@ -21,7 +21,19 @@ page](https://github.com/nodecopter/nodecopter.com/edit/master/pages/modules.md)
 * [copterface](https://github.com/paulhayes/copterface): Have your drone detecting faces using [node-opencv](https://github.com/peterbraden/node-opencv).
 * [qwertyhancockcopter](http://www.github.com/trodrigues/qwertyhancockcopter): Control your drone with a web based music keyboard.
 * [nodecopter-tennis](https://github.com/arjaneising/nodecopter-tennis): Use your drone as a tennis ball!
-* [ipad-ardrone-controller](https://github.com/createdotnet/ipad-ardrone-controller):Control your drone with an iPad and gestures.
+* [ipad-ardrone-controller](https://github.com/createdotnet/ipad-ardrone-controller): Control your drone with an iPad and gestures.
 * [qrar](https://npmjs.org/package/qrar): Decode QR codes found by your drone.
-* [node-drone-joystick](https://github.com/TooTallNate/node-drone-joystick): Control AR.Drones using any SDL-compatible Joystick (PS3 Sixaxis, etc.)
-* [drone-kinect](https://github.com/maxogden/drone-kinect): Control AR.Drones using your body (with a Kinect)
+
+
+<section>
+  <h2 id="npm-modules"><a href="#npm-modules">NodeCopter Modules on npm</a></h2>
+  <div class="modules-loading">
+    <p>Loading modules...</p>
+    <p><small>Might take some time on first load</small></p>
+  </div>
+  <ul class="modules-all"></ul>
+  <p><small><i>Module must be on npm with `nodecopter` in the name, description or as a keyword.</i></small></p>
+</section>
+<script src="/js/vendor/jquery.js"></script>
+<script src="/js/vendor/jquery-ajax-localstorage-cache.js"></script>
+<script src="/js/modules.js"></script>
---
+++ public/js/vendor/jquery-ajax-localstorage-cache.js
@@ -0,0 +1,56 @@
+// github.com/paulirish/jquery-ajax-localstorage-cache
+// dependent on Modernizr's localStorage test
+$.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+
+  // Cache it ?
+  if (!Modernizr.localstorage || !options.localCache) return;
+
+  var hourstl = options.cacheTTL || 5;
+
+  var cacheKey = options.cacheKey || options.url.replace(/jQuery.*/, '') + options.type + options.data;
+
+  // isCacheValid is a function to validate cache
+  if (options.isCacheValid && !options.isCacheValid()) {
+    localStorage.removeItem(cacheKey);
+  }
+  // if there's a TTL that's expired, flush this item
+  var ttl = localStorage.getItem(cacheKey + 'cachettl');
+  if (ttl && ttl < +new Date()) {
+    localStorage.removeItem(cacheKey);
+    localStorage.removeItem(cacheKey + 'cachettl');
+  }
+
+  var value = localStorage.getItem(cacheKey);
+  if (value) {
+    //In the cache? So get it, apply success callback & abort the XHR request
+    // parse back to JSON if we can.
+    if (options.dataType.indexOf('json') === 0) value = JSON.parse(value);
+    options.success(value);
+    // Abort is broken on JQ 1.5 :(
+    jqXHR.abort();
+  } else {
+
+    //If it not in the cache, we change the success callback, just put data on localstorage and after that apply the initial callback
+    if (options.success) {
+      options.realsuccess = options.success;
+    }
+    options.success = function(data) {
+      var strdata = data;
+      if (this.dataType.indexOf('json') === 0) strdata = JSON.stringify(data);
+
+      // Save the data to localStorage catching exceptions (possibly QUOTA_EXCEEDED_ERR)
+      try {
+        localStorage.setItem(cacheKey + 'cachettl', +new Date() + 1000 * 60 * 60 * hourstl);
+        localStorage.setItem(cacheKey, strdata);
+      } catch (e) {
+        // Remove any incomplete data that may have been saved before the exception was caught
+        localStorage.removeItem(cacheKey);
+        localStorage.removeItem(cacheKey + 'cachettl');
+        if (options.cacheError) options.cacheError(e, cacheKey, strdata);
+      }
+
+      if (options.realsuccess) options.realsuccess(data);
+    };
+
+  }
+});
