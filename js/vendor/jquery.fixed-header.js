5f9bbab1d44a26abda426c694883c005201a83c2
tim-kos <tim@debuggable.com>
2014-02-21 10:50:07 +0100

    get rid of the anchor scrolling hacking way and do it the right way using css

--- js/vendor/jquery.fixed-header.js
+++ js/vendor/jquery.fixed-header.js
@@ -16,36 +16,4 @@ $(function() {
       });
     }
   });
-
-  // fix onpage anchors
-  $('a').click(function(e) {
-    if ($(this).attr('href').indexOf('#') !== -1) {
-      e.preventDefault();
-
-      $('html, body').animate({
-          scrollTop: $($(this).attr('href')).offset().top - 70
-      }, 200);
-
-      var href = window.location.href;
-      var pos = href.indexOf('#');
-      if (pos === -1) {
-        window.location.href += $(this).attr('href');
-      } else {
-        window.location.href = href.substr(0, pos) + $(this).attr('href');
-      }
-    }
-  });
-
-  // scroll to the proper anchor if the browser location contains a hashbang
-  var href = window.location.href;
-  var pos  = href.indexOf('#');
-  if (pos !== -1) {
-    var name = href.substr(pos + 1);
-    var $target = $('#' + name);
-    if ($target.length > 0) {
-      $('html, body').animate({
-          scrollTop: $target.offset().top - 70
-      }, 200);
-    }
-  }
 });
